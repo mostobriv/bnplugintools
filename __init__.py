e3f4193e11951f15db4c5f2a98426d9ceeea3ee6
mostobriv <mostobriv@gmail.com>
2025-08-09 15:10:42 +0700

    initial

---
+++ __init__.py
@@ -0,0 +1,21 @@
+from .actions import (
+	Action,
+	PyToolsPluginCommand,
+	PyToolsUIAction,
+	ActionManager,
+	init_plugin_tools,
+	bn_action_manager,
+)
+
+# fmt: off
+__all__ = [
+	"init_plugin_tools",
+
+	"Action",
+	"PyToolsPluginCommand",
+	"PyToolsUIAction",
+
+	"ActionManager",
+	"bn_action_manager",
+]
+# fmt: on
---
+++ actions.py
@@ -0,0 +1,225 @@
+import binaryninja
+from binaryninja import PluginCommand, PluginCommandType
+from binaryninjaui import UIAction, UIActionContext, UIActionHandler  # type: ignore
+
+from typing import overload, Any
+
+
+# TODO: idk how to make the plugin name abstraction properly, change it later
+PLUGIN_NAME: str | None = None
+
+
+def init_plugin_tools(name: str):
+	global PLUGIN_NAME
+	PLUGIN_NAME = name
+
+	global bn_action_manager
+	bn_action_manager = ActionManager()
+
+	binaryninja.log.Logger(0, name)
+
+
+class Action:
+	"""
+	Action base class.
+
+	Attributes
+	----------
+	display_name: str
+		The string that is displayed in the menu.
+	description: str
+		Description of the action. Currently this is not used by BinaryNinja,
+		so it mostly for devs to understand.
+	desired_hotkey: str
+		Hotkey that is desired to bind to. This hotkey is used as a default in BinaryNinja.
+	logger: binaryninja.Logger
+		Logger object.
+	"""
+
+	display_name: str = ""
+	description: str = ""
+	desired_hotkey: str = ""
+	logger: binaryninja.Logger | None = None
+
+	def __init__(self):
+		assert PLUGIN_NAME is not None, "PLUGIN_NAME must be set"
+		self.logger = binaryninja.log.Logger(0, f"{PLUGIN_NAME}.{self.name}")
+
+	@property
+	def short_name(self):
+		return self.__class__.__name__
+
+	def register(self):
+		raise NotImplementedError
+
+	def unregister(self):
+		raise NotImplementedError
+
+
+class PyToolsPluginCommand(Action):
+	"""
+	Wrapper class for binaryninja.PluginCommand registration.
+
+	Attributes
+	----------
+	type: binaryninja.PluginCommandType
+		The type of plugincommand, used to decide which registration handler should use.
+	"""
+
+	type: PluginCommandType | None = None
+
+	_type_to_handler = {
+		PluginCommandType.DefaultPluginCommand: PluginCommand.register,
+		PluginCommandType.AddressPluginCommand: PluginCommand.register_for_address,
+		PluginCommandType.RangePluginCommand: PluginCommand.register_for_range,
+		PluginCommandType.FunctionPluginCommand: PluginCommand.register_for_function,
+		PluginCommandType.LowLevelILFunctionPluginCommand: PluginCommand.register_for_low_level_il_function,
+		PluginCommandType.LowLevelILInstructionPluginCommand: PluginCommand.register_for_low_level_il_instruction,
+		PluginCommandType.MediumLevelILFunctionPluginCommand: PluginCommand.register_for_medium_level_il_function,
+		PluginCommandType.MediumLevelILInstructionPluginCommand: PluginCommand.register_for_medium_level_il_instruction,
+		PluginCommandType.HighLevelILFunctionPluginCommand: PluginCommand.register_for_high_level_il_function,
+		PluginCommandType.HighLevelILInstructionPluginCommand: PluginCommand.register_for_high_level_il_instruction,
+		PluginCommandType.ProjectPluginCommand: PluginCommand.register_for_project,
+	}
+
+	def __init__(self):
+		super().__init__()
+
+		assert self.display_name and self.description, (
+			f"Fullname and description must be filled, missing for {self.short_name}"
+		)
+		assert self.type is not None, (
+			f"Plugin command type must be set, missing for {self.short_name}"
+		)
+
+	# fmt: off
+	@overload
+	def activate(self, bv: binaryninja.BinaryView): ...
+	@overload
+	def activate(self, bv: binaryninja.BinaryView, address: int): ...
+	@overload
+	def activate(self, bv: binaryninja.BinaryView, function: binaryninja.Function): ...
+	@overload
+	def activate(self, bv: binaryninja.BinaryView, hlil: binaryninja.HighLevelILFunction): ...
+	@overload
+	def activate(self, bv: binaryninja.BinaryView, hlil_instr: binaryninja.HighLevelILInstruction): ...
+	@overload
+	def activate(self, bv: binaryninja.BinaryView, mlil: binaryninja.MediumLevelILFunction): ...
+	@overload
+	def activate(self, bv: binaryninja.BinaryView, mlil_instr: binaryninja.MediumLevelILInstruction): ...
+	@overload
+	def activate(self, bv: binaryninja.BinaryView, llil: binaryninja.LowLevelILFunction): ...
+	@overload
+	def activate(self, bv: binaryninja.BinaryView, llil_instr: binaryninja.LowLevelILInstruction): ...
+	@overload
+	def activate(self, bv: binaryninja.BinaryView, address: int, length: int): ...
+
+	def activate(self, *args, **kwargs) -> Any:
+		raise NotImplementedError
+	# fmt: on
+
+	# fmt: off
+	@overload
+	def is_valid(self, bv: binaryninja.BinaryView) -> bool: ...
+	@overload
+	def is_valid(self, bv: binaryninja.BinaryView, address: int) -> bool: ...
+	@overload
+	def is_valid(self, bv: binaryninja.BinaryView, function: binaryninja.Function) -> bool: ...
+	@overload 
+	def is_valid(self, bv: binaryninja.BinaryView, llil: binaryninja.LowLevelILFunction) -> bool: ...
+	@overload 
+	def is_valid(self, bv: binaryninja.BinaryView, llil_instr: binaryninja.LowLevelILInstruction) -> bool: ...
+	@overload 
+	def is_valid(self, bv: binaryninja.BinaryView, mlil: binaryninja.MediumLevelILFunction) -> bool: ...
+	@overload 
+	def is_valid(self, bv: binaryninja.BinaryView, mlil_instr: binaryninja.MediumLevelILInstruction) -> bool: ...
+	@overload 
+	def is_valid(self, bv: binaryninja.BinaryView, hlil: binaryninja.HighLevelILFunction) -> bool: ...
+	@overload 
+	def is_valid(self, bv: binaryninja.BinaryView, hlil_instr: binaryninja.HighLevelILInstruction) -> bool: ...
+	@overload 
+	def is_valid(self, bv: binaryninja.BinaryView, begin: int, end: int) -> bool: ...
+
+	def is_valid(self, *args, **kwargs) -> bool:
+		raise NotImplementedError
+	# fmt: off
+
+	def register(self):
+		register = self.type_to_handler[self.type]
+
+		if self.desired_hotkey:
+			self.logger.log_warn(
+					f"Desired hotkey ({self.desired_hotkey}) for {self.short_name} is set, "
+					"but setting default hotkey currently unavailable for PluginCommands, ignored"
+				)
+
+		register(
+			f"{PLUGIN_NAME}\\{self.display_name}",
+			self.description,
+			self.activate,
+			self.is_valid,
+		)
+
+
+class PyToolsUIAction(Action):
+	def __init__(self):
+		super().__init__()
+		assert self.display_name and self.description, "Fullname and description must be filled"
+
+	# fmt: off
+	@overload
+	def activate(self): ...
+	@overload
+	def activate(self, context: UIActionContext): ...
+
+	def activate(self, *args, **kwargs) -> Any:
+		raise NotImplementedError
+	# fmt: on
+
+	# fmt: off
+	@overload
+	def is_valid(self) -> bool: return True
+	@overload
+	def is_valid(self, context: UIActionContext) -> bool: return True
+
+	def is_valid(self, *args, **kwargs) -> bool:
+		raise NotImplementedError
+	# fmt: on
+
+	def register(self):
+		UIAction.registerAction(f"{PLUGIN_NAME}\\{self.display_name}", self.desired_hotkey)
+		UIActionHandler.globalActions().bindAction(
+			f"{PLUGIN_NAME}\\{self.display_name}",
+			UIAction(self.activate, self.is_valid),
+		)
+
+
+class ActionManager:
+	def __init__(self, plugin_name: str | None = None) -> None:
+		self.__actions: list[Action] = list()
+
+		if not plugin_name:
+			plugin_name = PLUGIN_NAME
+
+		self.logger: binaryninja.Logger = binaryninja.log.Logger(
+			0, f"{plugin_name}.{self.__class__.__name__}"
+		)
+
+	def register(self, action: Action) -> None:
+		self.logger.log_info(f"Registering {action.short_name} action")
+
+		if not action.display_name or not action.description:
+			self.logger.log_error(
+				"Display name and description must be filled for action to be registered,"
+				f"missing for {action.short_name}"
+			)
+			return
+
+		self.__actions.append(action)
+		action.register()
+
+	def finalize(self) -> None:
+		raise NotImplementedError
+
+
+bn_action_manager: ActionManager | None = None
